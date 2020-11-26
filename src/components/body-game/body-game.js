c6ba0e35d1dbe80539735cc2c61d8161870448c5
Mikhail Korovai <korovai.personal@gmail.com>
2020-11-26 12:29:45 +0200

    Random non-repeating number generator

--- src/components/body-game/body-game.js
+++ src/components/body-game/body-game.js
@@ -11,8 +11,46 @@ import { Droppable } from "react-beautiful-dnd";
 import ListGameItems from '../list-game-items/list-game-items';
 
 class BodyGame extends Component {
+
+  getUnique(min, max, n) {
+    if (n > max-min+1 || n < 0) return [];
+    const buf = [];
+    
+    for (let i=0; i<n;) {
+      const value = Math.round(Math.random() * (max-min)) + min;
+      
+      if (buf.length === 0) {
+        buf[0] = value;
+        i++;
+      }
+  
+      if (buf.indexOf(value) === -1) {
+        buf.push(value);
+        i++;
+      }
+    }
+    
+    const res = [];
+    for (let i in buf) res[res.length] = buf[i];
+    return res;
+  };
+
   render() {
     const { classes, settings } = this.props;
+    const arrIndex = this.getUnique(0, 6, 7);
+
+    console.log('arrIndex: ', arrIndex);
+    console.log('arrIndex[1]: ', arrIndex[1]);
+
+    const newArr = [];
+
+    for (let i=0; i<arrIndex.length; i++) {
+      const item = settings.find((item, index) => index === arrIndex[i]);
+      newArr[i] = item;
+      console.log('item: ', item);
+    }
+
+    console.log(newArr);
     
     return (
       <Droppable droppableId="droppableElements">
@@ -24,7 +62,7 @@ class BodyGame extends Component {
           >
             <Box className={classes.playingField}>
               {
-                settings.map((item, index) => {
+                newArr.map((item, index) => {
                   return (
                     <ListGameItems
                       key={item.id}
